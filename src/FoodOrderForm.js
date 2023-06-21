47335622bc07a6f87872966a561d6b220fb9dad6
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-21 12:58:49 +0300

    adding value attributes to the inputs

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -10,18 +10,22 @@ function FoodOrderForm() {
       <label htmlFor="name">Name:</label>
       <input 
         id="name"
+        value={name}
       />
       <label htmlFor="phone">Phone:</label>
       <input
         id="phone"
+        value={phone}
       />
       <label htmlFor="address">Address:</label>
       <input
         id="address"
+        value={address}
       />
       <label htmlFor="order">Order:</label>
       <input
         id="order"
+        value={order}
       />
       <button type="submit">Submit Order</button>
     </form>
