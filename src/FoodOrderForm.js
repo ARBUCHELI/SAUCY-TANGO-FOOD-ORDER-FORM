fe1582078d71b8601348304134e75e00af321b8e
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-21 13:14:15 +0300

    adding name and type attribures to the input tags

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -26,24 +26,32 @@ function FoodOrderForm() {
         id="name"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        name="name"
+        type="text"
       />
       <label htmlFor="phone">Phone:</label>
       <input
         id="phone"
         value={phone}
-         onChange={(e) => setPhone(e.target.value)}
+        onChange={(e) => setPhone(e.target.value)}
+        name="phone"
+        type="text"
       />
       <label htmlFor="address">Address:</label>
       <input
         id="address"
         value={address}
         onChange={(e) => setAddress(e.target.value)}
+        name="address"
+        type="text"
       />
       <label htmlFor="order">Order:</label>
       <input
         id="order"
         value={order}
         onChange={(e) => setOrder(e.target.value)}
+        name="order"
+        type="text"
       />
       <button type="submit">Submit Order</button>
     </form>
