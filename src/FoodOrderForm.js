9fcd94870b861893caf0e4f79c338f7497f62e5c
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-29 12:15:14 +0300

    refactoring style

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./FoodOrderForm.module.css";
 
 function FoodOrderForm() {
   const [name, setName] = useState("");
@@ -20,40 +21,44 @@ function FoodOrderForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
+    <form onSubmit={handleSubmit} className={styles.mainform}>
+      <label htmlFor="name" className={styles.label}>Name:</label>
       <input 
         id="name"
         value={name}
         onChange={(e) => setName(e.target.value)}
         name="name"
         type="text"
+        className={styles.inputbox}
       />
-      <label htmlFor="phone">Phone:</label>
+      <label htmlFor="phone" className={styles.label}>Phone:</label>
       <input
         id="phone"
         value={phone}
         onChange={(e) => setPhone(e.target.value)}
         name="phone"
         type="text"
+        className={styles.inputbox}
       />
-      <label htmlFor="address">Address:</label>
+      <label htmlFor="address" className={styles.label}>Address:</label>
       <input
         id="address"
         value={address}
         onChange={(e) => setAddress(e.target.value)}
         name="address"
         type="text"
+        className={styles.inputbox}
       />
-      <label htmlFor="order">Order:</label>
+      <label htmlFor="order" className={styles.label}>Order:</label>
       <input
         id="order"
         value={order}
         onChange={(e) => setOrder(e.target.value)}
         name="order"
         type="text"
+        className={styles.inputbox}
       />
-      <button type="submit">Submit Order</button>
+      <button type="submit" className={styles.formbutton}>Submit Order</button>
     </form>
   )
 }
